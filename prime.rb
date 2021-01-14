46c90b0b3cc2f7a2d76489b9941d3e14eccb3639
Luke Gill <bagrad007@gmail.com>
2021-01-14 17:40:30 -0500

    commit tree e8b642863bb65e0e6e509c8675a8d1ae6f57ba5e

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
+require 'pry'
 # Add  code here!
+
+
+def prime?(nums)
+    # binding.pry
+    if nums.abs < 2
+        return false
+    end
+  (2..Math.sqrt(nums)).to_a.each do |n|
+        # binding.pry
+        if nums % n == 0
+            return false
+        end
+    end 
+    true
+end
